04529ad3743fee393e19f0b692ca468d98aaf156
Bibhas Bhattacharya <bibhas2@gmail.com>
2016-01-31 10:30:10 -0500

    Add unit tests.

--- WSKitTests/WSKitTests.swift
+++ WSKitTests/WSKitTests.swift
@@ -21,10 +21,164 @@ class WSKitTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testGET() {
+        let feature = self.expectationWithDescription("Test")
+        
+        WS.url("http://httpbin.org/get")
+            .get()
+            .then { (res) -> Void in
+                let body = res.body()
+                print(body)
+                let range = body.rangeOfString("Accept-Encoding")
+                XCTAssertNotEqual(range, nil)
+                feature.fulfill()
+            }.error {err in
+                print(err)
+                XCTFail("Got error")
+                feature.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(10) { (err) -> Void in
+            if (err != nil) {
+                XCTFail("Got timeout")
+            }
+        }
     }
+    
+    func testGETJSON() {
+        let feature = self.expectationWithDescription("Test")
+        
+        WS.url("http://httpbin.org/get")
+            .get()
+            .then { (res) -> Void in
+                do {
+                    let obj = try res.json() as! [String:AnyObject]
+                    
+                    XCTAssertEqual(obj["url"] as? String, Optional.Some("http://httpbin.org/get"))
+                } catch {
+                    XCTFail("Got error trying to get JSON")
+                }
+                feature.fulfill()
+            }.error {err in
+                print(err)
+                XCTFail("Got error")
+                feature.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(10) { (err) -> Void in
+            if (err != nil) {
+                XCTFail("Got timeout")
+            }
+        }
+    }
+    
+    func testGETBadJSON() {
+        let feature = self.expectationWithDescription("Test")
+        
+        WS.url("http://example.com")
+            .get()
+            .then { (res) -> Void in
+                do {
+                    try res.json()
+                    
+                    XCTFail("JSON parsing should have failed")
+                } catch {
+                    print("Got error trying to get JSON. This was expected.")
+                }
+                feature.fulfill()
+            }.error {err in
+                print(err)
+                XCTFail("Got error")
+                feature.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(10) { (err) -> Void in
+            if (err != nil) {
+                XCTFail("Got timeout")
+            }
+        }
+    }
+    
+    func testResponseStatus() {
+        let feature = self.expectationWithDescription("Test")
+        
+        //Make a GET request to a URL that only supports POST
+        WS.url("http://httpbin.org/post")
+            .get()
+            .then { (res) -> Void in
+                let body = res.body()
+                print(body)
+                XCTFail("This request should fail.")
+                feature.fulfill()
+            }.error {err in
+                //Error is expected
+                print(err)
+                feature.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(10) { (err) -> Void in
+            if (err != nil) {
+                XCTFail("Got timeout")
+            }
+        }
+    }
+    
+    func testJSONPOST() {
+        let feature = self.expectationWithDescription("Test")
+        
+        WS.url("http://httpbin.org/post")
+            .withHeaders(["Content-Type":"application/json"])
+            .post("{'hello':'mama'}")
+            .then { (res) -> Void in
+                let body = res.body()
+                print(body)
+                let range = body.rangeOfString("{'hello':'mama'}")
+                XCTAssertNotEqual(range, nil)
+                feature.fulfill()
+            }.error {err in
+                print(err)
+                XCTFail("Got error")
+                feature.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(10) { (err) -> Void in
+            if (err != nil) {
+                XCTFail("Got timeout")
+            }
+        }
+    }
+    
+    func testMapPOST() {
+        let m = ["planet" : "World", "population" : 7000000000]
+        let feature = self.expectationWithDescription("Test")
+        
+        do {
+            try WS.url("http://httpbin.org/post")
+                .withHeaders(["Content-Type":"application/json"])
+                .withBody(m)
+                .withMethod("POST")
+                .execute()
+                .then { (res) -> Void in
+                    let body = res.body()
+                    print(body)
+                    let range = body.rangeOfString("{\\\"planet\\\":\\\"World\\\",\\\"population\\\":7000000000}")
+                    XCTAssertNotEqual(range, nil)
+                    feature.fulfill()
+                }.error {err in
+                    print(err)
+                    XCTFail("Got error")
+                    feature.fulfill()
+            }
+        } catch {
+            XCTFail("Got exception")
+        }
+        self.waitForExpectationsWithTimeout(10) { (err) -> Void in
+            if (err != nil) {
+                XCTFail("Got timeout")
+            }
+        }
+    }
+    
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
@@ -32,5 +186,5 @@ class WSKitTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-    
+   
 }
